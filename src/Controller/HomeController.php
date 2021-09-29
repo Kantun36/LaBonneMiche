6d516f6bd893155e82d2f5dd7a4a1e2b6ed77c0d
Kantun <quentin.jouannot@gmail.com>
2021-09-29 11:36:39 +0200

    Titre et Icon sur le site

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,1 +1,15 @@
 <?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+class HomeController extends AbstractController
+{
+
+
+    public function home(): Response
+    {
+        return $this-> render('base.html.twig' , ['controller_name' => 'HomeController',
+            ]);
+    }
+}
