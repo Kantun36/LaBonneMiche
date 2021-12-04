ffeb402589345fe29d2b8145534a29d4b5b32982
Kantun <quentin.jouannot@gmail.com>
2021-12-04 16:57:54 +0100

    design

--- src/Controller/Admin/ProduitCrudController.php
+++ src/Controller/Admin/ProduitCrudController.php
@@ -2,8 +2,19 @@
 
 namespace App\Controller\Admin;
 
+use App\Controller\ImageController;
 use App\Entity\Produit;
+use APp\Entity\Image;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 
 class ProduitCrudController extends AbstractCrudController
 {
@@ -12,14 +23,38 @@ class ProduitCrudController extends AbstractCrudController
         return Produit::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('nomProduit'),
+            ChoiceField::new('Categorie')->setChoices(
+                [
+                    'pain'=>'pain',
+                    'baguette'=>'baguette',
+                    'miche'=>'miche',
+                    'vienoiserie individuel'=>'vienoiserie individuel',
+                    'vienoiserie en lot'=>'vienoiserie en lot',
+                    'sandwich'=>'sandwich',
+                    'dessert'=>'dessert',
+                    'salade'=>'salade',
+                    'plats chaud'=>'plats chaud',
+                    'patisserie individuel'=>'patisserie individuel',
+                    'gateau'=>'gateau',
+
+                ]
+            ),
+
+            TextEditorField::new('DescriptionProduit'),
+            NumberField::new('prixProduit'),
+            IntegerField::new('quantiteProduit'),
+            NumberField::new('poidsProduit'),
+            AssociationField::new('Ingredients'),
+            ImageField::new('image')->setUploadDir("public/img/produits")
+                ->setBasepath("public/img/produits"),
+
+
         ];
     }
-    */
+
 }
